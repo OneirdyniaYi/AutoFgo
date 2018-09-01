87252e842621aa5ad19021d357a9ed5b58a60e46
creeper121386 <32115448+creeper121386@users.noreply.github.com>
2018-09-01 16:16:42 +0800

    [9.1] second commit

--- config.py
+++ config.py
@@ -1,4 +1,23 @@
+# set full screen mode to get better effect.
 FULL_SCREEN = False
 
-# num 0~7, options: [all, saber, archer, lancer, rider, caster, assassin, berserker, special]
-SUPPORT = 7
+# num 0~8, options: [all, saber, archer, lancer, rider, caster, assassin, berserker, special]
+SUPPORT = 7     # default berserker
+
+# use ultimate skill or not:
+USE_ULTIMATE = False
+# time to sleep after using ultimate skills:
+ULTIMATE_SLEEP_TIME = 20
+
+# use servants' skill or not:
+USE_SKILL = True
+USED_SKILL = (0, 1, 2, 3, 4, 5, 6, 7, 8)     # reset the order of numbers to change skill orders.
+SKILL_SLEEP_TIME = 2      # time to sleep after using skills.
+
+
+# # use master's skill or not 
+# USE_MASTER_SKILL = True
+# # The round you want to use in, begin at 0.
+# MASTER_SKILL_ROUND = 1
+# MASTER_SKILL = (0, 1, 2)
+
