26217e6fdaa013e9e494d5dae860833080190887
creeper121386 <604029828@qq.com>
2018-12-27 23:08:45 +0800

    add

--- debug_grab.py
+++ debug_grab.py
@@ -8,10 +8,12 @@ input('x2, y2:')
 x2, y2 = autopy.mouse.location()
 ori = autopy.bitmap.capture_screen(((x1, y1), (x2-x1, y2-y1)))
 ori.save('./ori.png')
+i = 0
 while 1:
     now = autopy.bitmap.capture_screen(((x1, y1), (x2-x1, y2-y1)))
     now.save('./now.png')
-    print('running...')
+    i += 1
+    print('running:', i)
     if not (now == ori):
         break
     time.sleep(0.1)
